c7edd5762c33e8b7270b1814f2ba5151a69950d0
ashtemzere <116907766+ashtemzere@users.noreply.github.com>
2022-12-29 15:49:01 +0300

    Add files via upload

---
+++ assets/js/routes.js
@@ -0,0 +1,75 @@
+routes = [
+  {
+    path: "/main/",
+    url: "./index.html",
+  },
+  {
+    path: "/emulators/",
+    url: "./emulators.html",
+  },
+  {
+    path: "/entertainment/",
+    url: "./entertainment.html",
+  },
+  {
+    path: "/social/",
+    url: "./social.html",
+  },
+  {
+    path: "/tweaked/",
+    url: "./tweaked.html",
+  },
+  {
+    path: "/tweaked-app/",
+    url: "./tweaked-app.php",
+  },
+  {
+    path: "/other/",
+    url: "./other.html",
+  },
+  {
+    path: "/new/",
+    url: "./pages/new.html",
+  },
+  {
+    path: "/themes/",
+    url: "./themes.html",
+  },
+  {
+    path: "/theme-page/",
+    url: "./theme-page.php",
+  },
+  {
+    path: "/popular/",
+    url: "./popular.html",
+  },
+  {
+    path: "/tos/",
+    url: "./pages/tos.html",
+  },
+  {
+    path: "/gameplay/",
+    url: "./pages/gameplay.html",
+  },
+  {
+    path: "/eclipse/",
+    url: "./pages/eclipse.html",
+  },
+  {
+    path: "/uncover/",
+    url: "./pages/uncover.html",
+  },
+  {
+    path: "/aurora/",
+    url: "./pages/aurora.html",
+  },
+  {
+    path: "/app-page/",
+    url: "./app-page.php",
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: "(.*)",
+    url: "./pages/404.html",
+  },
+];
